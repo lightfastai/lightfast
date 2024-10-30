875a6d9bf8315587f46400540881cb71d804ae6a
jeevanpillay <169354619+jeevanpillay@users.noreply.github.com>
2024-10-30 11:49:39 +1100

    working version of render target from noise to limit

--- packages/webgl/src/index.ts
+++ packages/webgl/src/index.ts
@@ -1,4 +1,10 @@
 import type { PerlinNoiseShaderUniforms } from "./noise/perlin-noise-material";
-import { PerlinNoiseShaderMaterial } from "./noise/perlin-noise-material";
+import {
+  fragmentShader,
+  PerlinNoiseShaderMaterial,
+  vertexShader,
+} from "./noise/perlin-noise-material";
 
 export { PerlinNoiseShaderMaterial, type PerlinNoiseShaderUniforms };
+
+export { vertexShader, fragmentShader };
