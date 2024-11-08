284882669d55a993ea133fb60849e92959937192
jeevanpillay <169354619+jeevanpillay@users.noreply.github.com>
2024-11-08 19:08:34 +1100

    fixed scroll issue on landing page for dahlia

--- apps/dahlia/www/src/components/site-footer.tsx
+++ apps/dahlia/www/src/components/site-footer.tsx
@@ -4,7 +4,7 @@ export function SiteFooter() {
   return (
     <footer className="w-full py-6">
       <div className="container flex flex-row">
-        <p className="text-balance text-center text-sm leading-loose text-muted-foreground">
+        <p className="text-balance text-center text-xs leading-loose text-muted-foreground">
           {siteConfig.name} @ 2025
         </p>
       </div>
