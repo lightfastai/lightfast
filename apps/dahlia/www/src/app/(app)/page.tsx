8f6d664c9f5d5616a305e04b9140cf15d566bd31
jeevanpillay <169354619+jeevanpillay@users.noreply.github.com>
2024-11-08 16:49:02 +1100

    working on dahlia landing page. simple; booting up...

--- apps/dahlia/www/src/app/(app)/page.tsx
+++ apps/dahlia/www/src/app/(app)/page.tsx
@@ -1,12 +1,11 @@
-import { HydrateClient } from "~/trpc/server";
-import { GeometryAIGenerator } from "../components/geometry-ai-gen";
-
 export const runtime = "edge";
 
 export default function HomePage() {
   return (
-    <HydrateClient>
-      <GeometryAIGenerator />
-    </HydrateClient>
+    <div className="flex h-[calc(100vh-3rem)] w-full items-center justify-center">
+      <h1 className="font-mono text-sm font-bold uppercase tracking-widest">
+        Booting up...
+      </h1>
+    </div>
   );
 }
