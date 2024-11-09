e1220e05dc58202e216a9d66f0744ffe7a909dca
jeevanpillay <169354619+jeevanpillay@users.noreply.github.com>
2024-11-09 17:45:43 +1100

    added terms & privacy

---
+++ apps/dahlia/www/src/app/(app)/legal/layout.tsx
@@ -0,0 +1,7 @@
+interface LegalLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function LegalLayout({ children }: LegalLayoutProps) {
+  return <div className="container">{children}</div>;
+}
