4566f6d21b9691dcf20f1e4b977c241e0ddcf5b1
jeevanpillay <169354619+jeevanpillay@users.noreply.github.com>
2024-11-09 16:28:55 +1100

    now working sidebar installed

--- apps/dahlia/app/src/app/(app)/layout.tsx
+++ apps/dahlia/app/src/app/(app)/layout.tsx
@@ -1,10 +1,18 @@
+import { SidebarInset, SidebarProvider } from "@repo/ui/components/ui/sidebar";
+
+import { AppSidebar } from "~/components/app-sidebar";
 import { SiteFooter } from "~/components/site-footer";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      {children}
-      <SiteFooter />
+      <SidebarProvider>
+        <AppSidebar />
+        <SidebarInset>
+          {children}
+          <SiteFooter />
+        </SidebarInset>
+      </SidebarProvider>
     </>
   );
 }
