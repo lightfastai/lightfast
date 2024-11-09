6fecf0072452095e98f1505277cb138514f10e87
jeevanpillay <169354619+jeevanpillay@users.noreply.github.com>
2024-11-09 11:44:30 +1100

    removed unused packages from apps/dahlia/www/

--- apps/dahlia/www/src/app/layout.tsx
+++ apps/dahlia/www/src/app/layout.tsx
@@ -8,8 +8,6 @@ import { GeistSans } from "geist/font/sans";
 import { Toaster } from "@repo/ui/components/ui/toaster";
 import { cn } from "@repo/ui/lib/utils";
 
-import { TRPCReactProvider } from "../trpc/react";
-
 export const metadata: Metadata = {
   title: "Jeevan Pillay | Brand",
   description: "", // @TODO
@@ -31,11 +29,9 @@ export default function RootLayout({
           GeistMono.variable,
         )}
       >
-        <TRPCReactProvider>
-          <div className="relative flex min-h-screen flex-col bg-background">
-            {children}
-          </div>
-        </TRPCReactProvider>
+        <div className="relative flex min-h-screen flex-col bg-background">
+          {children}
+        </div>
         <Toaster />
       </body>
     </html>
