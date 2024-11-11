7c2cdafdc22225fcf92f6ac05f2d80435fa10d37
jeevanpillay <169354619+jeevanpillay@users.noreply.github.com>
2024-11-11 11:42:55 +1100

    removing unused files

--- apps/dahlia/app/src/app/(app)/(experimental)/(playground)/playground/page.tsx
+++ apps/dahlia/app/src/app/(app)/(experimental)/(playground)/playground/page.tsx
@@ -1,10 +1,8 @@
-import { AppLayout } from "@repo/ui/components/app-layout";
-
 import { TextureSelectorCombobox } from "./components/texture-selector-combobox";
 
 export default function TexturePage() {
   return (
-    <AppLayout>
+    <>
       <header className="flex items-center justify-between">
         <h1 className="font-mono text-xs font-extrabold uppercase tracking-widest text-muted-foreground">
           Texture: <TextureSelectorCombobox />
@@ -12,6 +10,6 @@ export default function TexturePage() {
       </header>
 
       <div className="flex h-full w-full border">hi</div>
-    </AppLayout>
+    </>
   );
 }
