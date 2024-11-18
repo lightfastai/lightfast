68dc9dce5c41a795b736d8d485103c5594e55e77
jeevanpillay <169354619+jeevanpillay@users.noreply.github.com>
2024-11-18 13:28:41 +1100

    reintroduced pan-offset

--- apps/dahlia/app/src/app/(app)/(stable)/(network-editor)/components/workspace/use-cursor-position.ts
+++ apps/dahlia/app/src/app/(app)/(stable)/(network-editor)/components/workspace/use-cursor-position.ts
@@ -5,12 +5,14 @@ interface UseCursorPositionProps {
   canvasRef: RefObject<HTMLDivElement>;
   zoom: number;
   gridSize: number;
+  panOffset: { x: number; y: number };
 }
 
 export const useCursorPosition = ({
   canvasRef,
   zoom,
   gridSize,
+  panOffset,
 }: UseCursorPositionProps) => {
   const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
 
@@ -19,8 +21,10 @@ export const useCursorPosition = ({
       const rect = canvasRef.current.getBoundingClientRect();
       const canvas = canvasRef.current;
 
-      const x = (e.clientX - rect.left + canvas.scrollLeft) / zoom;
-      const y = (e.clientY - rect.top + canvas.scrollTop) / zoom;
+      const x =
+        (e.clientX - rect.left + canvas.scrollLeft) / zoom - panOffset.x / zoom;
+      const y =
+        (e.clientY - rect.top + canvas.scrollTop) / zoom - panOffset.y / zoom;
 
       const snappedX = Math.floor(x / gridSize) * gridSize;
       const snappedY = Math.floor(y / gridSize) * gridSize;
