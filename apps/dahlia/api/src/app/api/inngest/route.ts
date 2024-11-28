ce75c0db75c7966e7906459c1cad07434c390c39
jeevanpillay <169354619+jeevanpillay@users.noreply.github.com>
2024-11-28 11:13:30 +1100

    added inggest dev config

---
+++ apps/dahlia/api/src/app/api/inngest/route.ts
@@ -0,0 +1,6 @@
+import { createInngestEventContext } from "@repo/events/root";
+
+import { helloWorld } from "~/inngest/test";
+
+// Create an API that serves zero functions
+export const { GET, POST, PUT } = createInngestEventContext([helloWorld]);
