51b1d16e3fa8eb15c86a6238995ca5f3e40d1c7d
jeevanpillay <169354619+jeevanpillay@users.noreply.github.com>
2024-12-03 10:35:06 +1100

    added protectedtenantprocedure and init tenant db in trpc

--- apps/dahlia/db/src/app/src/client.ts
+++ apps/dahlia/db/src/app/src/client.ts
@@ -1,5 +1,20 @@
-import { createDbClient } from "@vendor/db";
+import {
+  createApiClient,
+  createDbClient,
+  getDatabaseUri as getDatabaseUriShared,
+} from "@vendor/db";
 
 import { env } from "~/env";
 
 export const db = createDbClient(env.DAHLIA_APP_DB_URL);
+
+export const apiClient = createApiClient(env.DAHLIA_APP_NEON_API_KEY);
+
+export const getDatabaseUri = async (projectId: string) => {
+  return getDatabaseUriShared(
+    apiClient,
+    projectId,
+    env.DAHLIA_APP_DB_NAME,
+    env.DAHLIA_APP_ROLE_NAME,
+  );
+};
--- vendor/db/src/shared/client.ts
+++ vendor/db/src/shared/client.ts
@@ -1,3 +1,4 @@
+import { createApiClient as createNeonApiClient } from "@neondatabase/api-client";
 import { drizzle } from "drizzle-orm/neon-serverless";
 
 import type { Db } from "../types/db";
@@ -7,4 +8,28 @@ export const createDbClient = (uri: string): Db => {
     connection: uri,
     casing: "snake_case",
   });
+};
+
+export const createApiClient = (apiKey: string) =>
+  createNeonApiClient({
+    apiKey,
+  });
+
+export const getDatabaseUri = async (
+  neonApiClient: ReturnType<typeof createApiClient>,
+  projectId: string,
+  db_name: string,
+  role_name: string,
+) => {
+  try {
+    const { data } = await neonApiClient.getConnectionUri({
+      projectId,
+      database_name: db_name,
+      role_name,
+    });
+    return data.uri;
+  } catch (error) {
+    console.error(error);
+    throw new Error("Failed to get database URI");
+  }
 };
