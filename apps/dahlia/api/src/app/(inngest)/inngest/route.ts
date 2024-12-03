86013af447433d1a802040b03a56e45536d1a97c
jeevanpillay <169354619+jeevanpillay@users.noreply.github.com>
2024-12-03 18:57:13 +1100

    moved trpc back to app

--- apps/dahlia/api/src/app/(inngest)/inngest/route.ts
+++ apps/dahlia/api/src/app/(inngest)/inngest/route.ts
@@ -1,7 +1,7 @@
 import { serve } from "inngest/next";
 
 import { inngest } from "~/inngest/client";
-import { handleCreateUser } from "./functions/handle-create-user";
+import { handleCreateUser } from "./_functions/handle-create-user";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
