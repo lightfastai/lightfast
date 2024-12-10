27f825a2cc9dad1e2dafa1666f95d7f490e1138d
jeevanpillay <169354619+jeevanpillay@users.noreply.github.com>
2024-12-11 10:32:22 +1100

    added custom app env called APP_ENV

--- vendor/next/env.ts
+++ vendor/next/env.ts
@@ -5,6 +5,7 @@ import { z } from "zod";
 export const env = createEnv({
   extends: [vercel()],
   shared: {
+    APP_ENV: z.enum(["prod", "staging", "preview", "dev"]).default("dev"), // Custom env for the app based on our dev env
     NODE_ENV: z
       .enum(["development", "production", "test"])
       .default("development"),
@@ -19,6 +20,7 @@ export const env = createEnv({
     NEXT_PUBLIC_SENTRY_DSN: z.string().min(1),
   },
   experimental__runtimeEnv: {
+    APP_ENV: process.env.APP_ENV,
     NODE_ENV: process.env.NODE_ENV,
     CI: process.env.CI,
     NEXT_PUBLIC_SENTRY_DSN: process.env.NEXT_PUBLIC_SENTRY_DSN,
