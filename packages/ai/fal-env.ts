61b5f3054d8b36de790df2e818980b00e3619901
jeevanpillay <169354619+jeevanpillay@users.noreply.github.com>
2024-12-11 12:33:45 +1100

    created two env for better seperation of concern between fal & llm

---
+++ packages/ai/fal-env.ts
@@ -0,0 +1,12 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const falEnv = createEnv({
+  server: {
+    FAL_KEY: z.string().min(1),
+  },
+  client: {},
+  experimental__runtimeEnv: {},
+  skipValidation:
+    !!process.env.CI || process.env.npm_lifecycle_event === "lint",
+});
--- packages/ai/llm-env.ts
+++ packages/ai/llm-env.ts
@@ -1,16 +1,6 @@
 import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
-export const falEnv = createEnv({
-  server: {
-    FAL_KEY: z.string().min(1),
-  },
-  client: {},
-  experimental__runtimeEnv: {},
-  skipValidation:
-    !!process.env.CI || process.env.npm_lifecycle_event === "lint",
-});
-
 export const llmEnv = createEnv({
   server: {
     OPENAI_API_KEY: z.string().min(1).startsWith("sk-proj-"),
