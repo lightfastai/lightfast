637d40a8d6f7aedb77a5bbab4aa2f460f84e21e7
jeevanpillay <169354619+jeevanpillay@users.noreply.github.com>
2024-12-11 16:58:19 +1100

    now redis executor created

--- vendor/observability/src/types.ts
+++
@@ -1,6 +0,0 @@
-export interface Logger {
-  info: (message: string, ...args: any[]) => void;
-  error: (message: string, ...args: any[]) => void;
-  warn: (message: string, ...args: any[]) => void;
-  debug: (message: string, ...args: any[]) => void;
-}
