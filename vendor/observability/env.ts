e3ac2b4523be05b4968a9892871024afa91b5186
jeevanpillay <169354619+jeevanpillay@users.noreply.github.com>
2024-12-15 10:41:32 +1100

    now logger works if vercel

--- vendor/observability/env.ts
+++ vendor/observability/env.ts
@@ -1,13 +1,16 @@
 import { createEnv } from "@t3-oss/env-nextjs";
+import { vercel } from "@t3-oss/env-nextjs/presets";
 import { z } from "zod";
 
 export const env = createEnv({
+  extends: [vercel()],
   shared: {
     NODE_ENV: z
       .enum(["development", "production", "test"])
       .default("development"),
   },
   server: {
+    VERCEL: z.string().optional(),
     LOGTAIL_SOURCE_TOKEN: z.string().min(1),
   },
   client: {},
--- vendor/observability/src/log.ts
+++ vendor/observability/src/log.ts
@@ -2,6 +2,6 @@ import { log as logtail } from "@logtail/next";
 
 import { env } from "../env";
 
-export const log = env.NODE_ENV === "production" ? logtail : console;
+export const log = env.VERCEL ? logtail : console;
 
 export type Logger = typeof log;
