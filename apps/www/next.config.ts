8de74a7668764c4cf7cf945685cdb50dcac76b34
jeevanpillay <169354619+jeevanpillay@users.noreply.github.com>
2025-04-17 14:01:45 +1000

    chore: update pnpm-lock and workspace configuration to use React 19.1.0 and Next.js 15.3.0; remove obsolete Next.js configuration and not-found page; adjust package.json to reference catalog for React and Next.js dependencies

--- apps/www/next.config.ts
+++ apps/www/next.config.ts
@@ -1,11 +1,9 @@
-import { fileURLToPath } from "url";
-import createJiti from "jiti";
+import { NextConfig } from "next";
 
-// Import env files to validate at build time. Use jiti so we can load .ts files in here.
-createJiti(fileURLToPath(import.meta.url))("./src/env");
+// @todo import the env.ts file here...
 
 /** @type {import("next").NextConfig} */
-const config = {
+const config: NextConfig = {
   reactStrictMode: true,
 
   /** Enables hot reloading for local packages without a build step */
@@ -20,3 +18,5 @@ const config = {
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
 };
+
+export default config;
