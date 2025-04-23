fdce6484cb4c9bc86a78572339f56e39cec37dcb
jeevanpillay <169354619+jeevanpillay@users.noreply.github.com>
2025-04-23 21:44:22 +1000

    refactor: extend environment configuration to include Sentry observability features for enhanced error tracking

--- vendor/next/env.ts
+++ vendor/next/env.ts
@@ -1,8 +1,10 @@
 import { createEnv } from "@t3-oss/env-nextjs";
 import { vercel } from "@t3-oss/env-nextjs/presets-zod";
 
+import { sentryEnv } from "@vendor/observability/env";
+
 export const env = createEnv({
-  extends: [vercel()],
+  extends: [vercel(), sentryEnv],
   shared: {},
   server: {
     // Added by Vercel
