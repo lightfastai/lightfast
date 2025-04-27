4446feb6902b21ffded61684c28fe2023c6e965f
jeevanpillay <169354619+jeevanpillay@users.noreply.github.com>
2025-04-27 22:28:51 +1000

    feat: enhance media server resource schema with new job and resource definitions, incorporating Zod validation and default ID generation

--- vendor/db/src/media-server/schema/tables/Resource.ts
+++ vendor/db/src/media-server/schema/tables/Resource.ts
@@ -1,9 +1,48 @@
-import { pgSchema, varchar } from "drizzle-orm/pg-core";
+import { jsonb, pgSchema, varchar } from "drizzle-orm/pg-core";
+import { z } from "zod";
+
+import { nanoid } from "@repo/lib";
 
 // Define the schema
 export const mediaServerSchema = pgSchema("media_server");
 
+export const $MediaServerJobStatus = z.enum([
+  "PROCESSING",
+  "COMPLETED",
+  "FAILED",
+]);
+
+export const $MediaServerProcessorEngine = z.enum(["fal-ai/fast-sdxl"]);
+
+export type MediaServerJobStatus = z.infer<typeof $MediaServerJobStatus>;
+export type MediaServerProcessorEngine = z.infer<
+  typeof $MediaServerProcessorEngine
+>;
+
+export const $MediaServerResourceData = z.object({
+  prompt: z.string(),
+});
+
+export type MediaServerResourceData = z.infer<typeof $MediaServerResourceData>;
+
 // Define the table within the schema
 export const MediaServerResource = mediaServerSchema.table("resource", {
-  id: varchar("id", { length: 191 }).notNull().primaryKey(),
+  id: varchar("id", { length: 191 })
+    .notNull()
+    .primaryKey()
+    .$defaultFn(() => nanoid()),
+  engine: varchar("engine", { length: 255 })
+    .notNull()
+    .$type<MediaServerProcessorEngine>(),
+  data: jsonb("data").notNull().$type<typeof $MediaServerResourceData>(),
+});
+
+export const MediaServerJob = mediaServerSchema.table("job", {
+  id: varchar("id", { length: 191 })
+    .notNull()
+    .primaryKey()
+    .$defaultFn(() => nanoid()),
+  status: varchar("status", { length: 255 })
+    .notNull()
+    .$type<MediaServerJobStatus>(),
 });
