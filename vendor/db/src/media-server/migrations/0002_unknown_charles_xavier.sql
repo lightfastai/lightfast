f45eaad8cae353df2c252ec893e1d165a723fc10
jeevanpillay <169354619+jeevanpillay@users.noreply.github.com>
2025-04-28 15:54:15 +1000

    chore: update media server migration to add 'status' column and grant permissions for schema access and table operations

--- vendor/db/src/media-server/migrations/0002_unknown_charles_xavier.sql
+++ vendor/db/src/media-server/migrations/0002_unknown_charles_xavier.sql
@@ -4,3 +4,6 @@ CREATE TYPE "public"."media_server_resource_type" AS ENUM('image', 'video', 'aud
 ALTER TABLE "media_server"."resource" ALTER COLUMN "engine" SET DATA TYPE "public"."media_server_processor_engine" USING "engine"::"public"."media_server_processor_engine";--> statement-breakpoint
 ALTER TABLE "media_server"."resource" ALTER COLUMN "type" SET DATA TYPE "public"."media_server_resource_type" USING "type"::"public"."media_server_resource_type";--> statement-breakpoint
 ALTER TABLE "media_server"."resource" ADD COLUMN "status" "media_server_job_status" NOT NULL;
+GRANT USAGE ON SCHEMA media_server TO anon, authenticated;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA media_server TO anon, authenticated;
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA media_server TO anon, authenticated;
