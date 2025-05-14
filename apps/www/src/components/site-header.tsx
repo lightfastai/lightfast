9bc729b766f5a4bf808d9894cdae577c171d80b8
jeevanpillay <169354619+jeevanpillay@users.noreply.github.com>
2025-05-14 12:45:18 +1000

    feat: update site header to use site configuration for branding and enhance text styling

--- apps/www/src/components/site-header.tsx
+++ apps/www/src/components/site-header.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { Container } from "@repo/ui/components/ui/container";
 
 import { Icons } from "~/components/icons";
+import { siteConfig } from "~/config/site";
 
 export function SiteHeader() {
   return (
@@ -12,9 +13,9 @@ export function SiteHeader() {
           <div className="flex items-center gap-2">
             <Link href="/" className="flex items-center space-x-2">
               <Icons.logo className="size-4" />
-              <h1 className="font-base text-lg">
-                <span>Lightfast</span>{" "}
-                <span className="bg-gradient-to-r from-sky-400 via-fuchsia-400 to-orange-400 bg-clip-text text-transparent">
+              <h1 className="text-md py-2 font-semibold">
+                <span className="">{siteConfig.name}</span>{" "}
+                <span className="relative inline-block bg-gradient-to-r from-sky-400 via-fuchsia-400 to-orange-400 bg-clip-text font-mono text-transparent">
                   Computer
                 </span>
               </h1>
