1e0d8188ea4373825780ec267eaacfea7b3acd46
Jeevan Pillay <jeevanpillay@iv-macbook.modem>
2025-08-01 21:29:46 +1000

    Updating the app layout to include sidebar components

--- apps/app/src/app/(app)/layout.tsx
+++ apps/app/src/app/(app)/layout.tsx
@@ -1,7 +1,15 @@
+import { AppSidebar } from "~/components/app-sidebar";
+import { SidebarInset, SidebarProvider } from "@repo/ui/components/ui/sidebar";
+
 export default function AppLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <>{children}</>;
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>{children}</SidebarInset>
+    </SidebarProvider>
+  );
 }
