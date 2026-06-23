b185efef2c9d31b60fc5b6fee5ccb9fe74bc3464
Jeevan Pillay <169354619+jeevanpillay@users.noreply.github.com>
2026-06-23 10:35:23 +1000

    Enable Sentry logging for newsletter action

--- vendor/observability/src/sentry-nextjs.ts
+++ vendor/observability/src/sentry-nextjs.ts
@@ -1,7 +1,11 @@
-export {
-  captureConsoleIntegration,
-  captureRequestError,
-  extraErrorDataIntegration,
-  init,
-  spotlightIntegration,
-} from "@sentry/nextjs";
+import Sentry from "@sentry/nextjs";
+
+export const captureException = Sentry.captureException;
+export const captureRequestError = Sentry.captureRequestError;
+export const consoleLoggingIntegration = Sentry.consoleLoggingIntegration;
+export const extraErrorDataIntegration = Sentry.extraErrorDataIntegration;
+export const init = Sentry.init;
+export const logger = Sentry.logger;
+export const spotlightIntegration = Sentry.spotlightIntegration;
+export const withServerActionInstrumentation =
+  Sentry.withServerActionInstrumentation;
